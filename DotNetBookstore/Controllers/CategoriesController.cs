f7914f7a2dfa3973ff2fea8ba03ed292204ed419
Dario Hesami <dario.hesami@hotmail.com>
2026-06-04 17:51:41 -0400

    W05-P1-Scaffolding-Categories

--- DotNetBookstore/Controllers/CategoriesController.cs
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -1,75 +1,150 @@
-﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using DotNetBookstore.Models;
+using DotNetBookstore.Data;
 
-namespace DotNetBookstore.Controllers
+public class CategoriesController : Controller
 {
-    public class CategoriesController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public CategoriesController(ApplicationDbContext context)
     {
-        public IActionResult Index()
+        _context = context;
+    }
+
+    // GET: CATEGORYS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.Categories.ToListAsync());
+    }
+
+    // GET: CATEGORYS/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            // You can add any logic here that you want to execute when the categories page is accessed.
-            // This is where you would typically retrieve data from a database or perform any other operations that are necessary for the page to function properly. You can also pass data to the view using the ViewBag or ViewData properties, or by creating a view model and passing it to the view.
-            // This could include things like retrieving data from a database, performing calculations, or any other operations that are necessary for the page to function properly.
+            return NotFound();
+        }
 
-            // For demonstration purposes, we'll just create a list of categories in memory and pass it to the view.         
-            //var categories = new List<Category>
-            // {
-            //     new Models.Category { CategoryId = 1, Name = "Novels" },
-            //     new Models.Category { CategoryId = 2, Name = "History" },
-            //     new Models.Category { CategoryId = 3, Name = "Science Fiction" }
-            // };
+        var category = await _context.Categories
+            .FirstOrDefaultAsync(m => m.CategoryId == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
 
-            // In a real application, you would typically retrieve the categories from a database
-            // For demonstration purposes, we'll just create a list of categories in memory
-            var categories = new List<Category>();
-            for (int i = 1; i <= 10; i++)
+        return View(category);
+    }
+
+    // GET: CATEGORYS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: CATEGORYS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("CategoryId,Name")] Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    // GET: CATEGORYS/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    // POST: CATEGORYS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, [Bind("CategoryId,Name")] Category category)
+    {
+        if (id != category.CategoryId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
             {
-                categories.Add(new Category { CategoryId = i, Name = $"Category {i}" });
+                _context.Update(category);
+                await _context.SaveChangesAsync();
             }
-            // Pass the list of categories to the view (strongly-typed)
-            // model binding: the view will be strongly-typed to a list of Category objects, and we will pass the list of categories to the view using the View method. This allows us to use the categories in the view to display them to the user.
-            return View(categories);
-        }
-
-        // This action method will be called when the user clicks on a category link. It will receive the category name as a parameter and can use it to retrieve the relevant books from the database or perform any other necessary operations.
-        public IActionResult Browse(string category)
-        {
-            // A simple error handling: if the category parameter is null or empty, we can return a BadRequest result or redirect to an error page.
-            if (string.IsNullOrEmpty(category))
+            catch (DbUpdateConcurrencyException)
             {
-                // Return a BadRequest result with a message indicating that the category cannot be null or empty.
-                // This will result in a 400 Bad Request response being sent to the client, along with the specified error message.
-                // You can also choose to redirect to an error page instead of returning a BadRequest result, depending on how you want to handle errors in your application.
-                // for demonstration purposes, we'll just return a BadRequest result with a message indicating that the category cannot be null or empty (in production, you might want to log this error or handle it differently).
-                //return BadRequest("Category cannot be null or empty.");
-
-                // Redirect to the Index action of the CategoriesController, which will display the list of categories. This is a common way to handle errors in a user-friendly way, by redirecting the user back to a relevant page instead of showing an error message.
-                return RedirectToAction("Index");
-            }
-
-            // passing the category name to the view using ViewBag (weakly-typed)
-            ViewBag.Category = category;
-            // In a real application, you would typically retrieve the books for the specified category from a database and pass them to the view. For demonstration purposes, we'll just pass the category name to the view using ViewBag.
-            return View();
-        }
-
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Category category)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(category);
+                if (!CategoryExists(category.CategoryId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
             }
             return RedirectToAction(nameof(Index));
         }
-
-
+        return View(category);
     }
 
+    // GET: CATEGORYS/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await _context.Categories
+            .FirstOrDefaultAsync(m => m.CategoryId == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    // POST: CATEGORYS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category != null)
+        {
+            _context.Categories.Remove(category);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CategoryExists(int? id)
+    {
+        return _context.Categories.Any(e => e.CategoryId == id);
+    }
 }
